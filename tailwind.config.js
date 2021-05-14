c25058fe1d8983c62845b8c3c33c7ad49251c4a9
DeriFirgiawan <derifirgiawan025@gmail.com>
2021-05-14 19:50:14 +0700

    add config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,32 @@
 module.exports = {
-  purge: [],
+  purge: ["./public/**/*.html","./public/**/*.js"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    screens: {
+      'md': {'max': '991.98px'}
+    },
+    extend: {
+      screens: {
+        'xs': {'max': '575.98px'}
+      },
+      fontSize: {
+        '5xl': ['48px', {
+          lineHeight: '58px'
+        }]
+      },
+      colors: {
+        warning: {
+          "500": "#FFB61D",
+          "600": "#E9A10C"
+        }
+      }
+    },
   },
   variants: {
-    extend: {},
+    extend: {
+      fontWeight: ['responsive', 'hover'],
+      fontSize: ['hover']
+    },
   },
   plugins: [],
 }
